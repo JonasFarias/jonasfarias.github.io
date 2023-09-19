5ed6fa16a7a74b07c500d965db3d3ac7b9cde345
Jonas Farias <jhonas.jr@hotmail.com>
2023-09-19 18:35:52 -0300

    testando

--- scripts/mododark.js
+++ scripts/mododark.js
@@ -1,5 +1,12 @@
 let trilho = document.getElementById('trilho')
+let nav = document.querySelector('nav')
+let header = document.querySelector('header')
+let body = document.querySelector('body')
 
 trilho.addEventListener('click', ()=>{
+    body.classList.toggle('dark')
     trilho.classList.toggle('dark')
+    nav.classList.toggle('dark')
+    hearder.classList.toggle('dark') 
+    
 })
