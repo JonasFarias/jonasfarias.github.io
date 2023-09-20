2b8fbb9e021dbb591de0deaab01674845b3aed1c
Jonas Farias <jhonas.jr@hotmail.com>
2023-09-20 18:06:52 -0300

    .

--- scripts/mododark.js
+++ scripts/mododark.js
@@ -2,11 +2,14 @@ let trilho = document.getElementById('trilho')
 let nav = document.querySelector('nav')
 let header = document.querySelector('header')
 let body = document.querySelector('body')
+let secondary__title = document.querySelector('secondary__title')
+
 
 trilho.addEventListener('click', ()=>{
     body.classList.toggle('dark')
     trilho.classList.toggle('dark')
     nav.classList.toggle('dark')
-    hearder.classList.toggle('dark') 
+    hearder.classList.toggle('dark')
+    secondary__title.classList.toggle('dark')
     
 })
